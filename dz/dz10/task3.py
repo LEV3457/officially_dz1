9d4bca0818df21b884bc3e0a138f89eec530608c
LEV3457 <lukyanovivan83@gmail.com>
2024-12-06 18:51:54 +0000

    сделано

---
+++ dz/dz10/task3.py
@@ -0,0 +1,8 @@
+def number():
+    for i in range(1,11):
+        for r in range(start_mul, ):
+            print(f"{i} * {r} = {i * r}")
+        print()
+multiplication_table()
+
+def multiplication_table()
